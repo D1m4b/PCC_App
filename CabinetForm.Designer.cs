20e1c0edeca52ec28b1009974d8ab964c8f92874
D1m4b <dmytri.2019@gmail.com>
2026-06-16 12:06:32 +0400

    Update CabinetForm.Designer.cs

--- CabinetForm.Designer.cs
+++ CabinetForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace PCC_App
+namespace PCC_App
 {
     partial class CabinetForm
     {
@@ -52,8 +52,13 @@
             this.btnTopUp = new System.Windows.Forms.Button();
             this.txtTopUpAmount = new System.Windows.Forms.TextBox();
             this.panelMain = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.dtpBookingTime = new System.Windows.Forms.DateTimePicker();
+            this.dtpBookingDate = new System.Windows.Forms.DateTimePicker();
             this.cmbHours = new System.Windows.Forms.ComboBox();
             this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
             this.panelTopUp.SuspendLayout();
             this.panelMain.SuspendLayout();
@@ -63,7 +68,7 @@
             // 
             this.lblNickname.AutoSize = true;
             this.lblNickname.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblNickname.Location = new System.Drawing.Point(56, 86);
+            this.lblNickname.Location = new System.Drawing.Point(140, 86);
             this.lblNickname.Name = "lblNickname";
             this.lblNickname.Size = new System.Drawing.Size(75, 21);
             this.lblNickname.TabIndex = 0;
@@ -73,7 +78,7 @@
             // 
             this.lblBalance.AutoSize = true;
             this.lblBalance.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblBalance.Location = new System.Drawing.Point(56, 143);
+            this.lblBalance.Location = new System.Drawing.Point(140, 134);
             this.lblBalance.Name = "lblBalance";
             this.lblBalance.Size = new System.Drawing.Size(59, 21);
             this.lblBalance.TabIndex = 2;
@@ -81,10 +86,11 @@
             // 
             // dgvHistory
             // 
+            this.dgvHistory.BackgroundColor = System.Drawing.Color.White;
             this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHistory.Location = new System.Drawing.Point(402, 176);
+            this.dgvHistory.Location = new System.Drawing.Point(402, 164);
             this.dgvHistory.Name = "dgvHistory";
-            this.dgvHistory.Size = new System.Drawing.Size(450, 183);
+            this.dgvHistory.Size = new System.Drawing.Size(576, 232);
             this.dgvHistory.TabIndex = 3;
             // 
             // btnShowHistory
@@ -93,7 +99,7 @@
             this.btnShowHistory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnShowHistory.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnShowHistory.ForeColor = System.Drawing.Color.White;
-            this.btnShowHistory.Location = new System.Drawing.Point(402, 92);
+            this.btnShowHistory.Location = new System.Drawing.Point(552, 72);
             this.btnShowHistory.Name = "btnShowHistory";
             this.btnShowHistory.Size = new System.Drawing.Size(174, 40);
             this.btnShowHistory.TabIndex = 4;
@@ -104,9 +110,9 @@
             // cmbHalls
             // 
             this.cmbHalls.FormattingEnabled = true;
-            this.cmbHalls.Location = new System.Drawing.Point(164, 134);
+            this.cmbHalls.Location = new System.Drawing.Point(163, 134);
             this.cmbHalls.Name = "cmbHalls";
-            this.cmbHalls.Size = new System.Drawing.Size(121, 21);
+            this.cmbHalls.Size = new System.Drawing.Size(200, 21);
             this.cmbHalls.TabIndex = 5;
             this.cmbHalls.SelectedIndexChanged += new System.EventHandler(this.cmbHalls_SelectedIndexChanged);
             // 
@@ -115,7 +121,7 @@
             this.cmbComputers.FormattingEnabled = true;
             this.cmbComputers.Location = new System.Drawing.Point(164, 227);
             this.cmbComputers.Name = "cmbComputers";
-            this.cmbComputers.Size = new System.Drawing.Size(121, 21);
+            this.cmbComputers.Size = new System.Drawing.Size(199, 21);
             this.cmbComputers.TabIndex = 6;
             // 
             // cmbTariffs
@@ -123,7 +129,7 @@
             this.cmbTariffs.FormattingEnabled = true;
             this.cmbTariffs.Location = new System.Drawing.Point(164, 179);
             this.cmbTariffs.Name = "cmbTariffs";
-            this.cmbTariffs.Size = new System.Drawing.Size(121, 21);
+            this.cmbTariffs.Size = new System.Drawing.Size(199, 21);
             this.cmbTariffs.TabIndex = 7;
             this.cmbTariffs.SelectedIndexChanged += new System.EventHandler(this.cmbTariffs_SelectedIndexChanged);
             // 
@@ -131,7 +137,7 @@
             // 
             this.txtTotalPrice.Location = new System.Drawing.Point(184, 315);
             this.txtTotalPrice.Name = "txtTotalPrice";
-            this.txtTotalPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalPrice.Size = new System.Drawing.Size(179, 20);
             this.txtTotalPrice.TabIndex = 8;
             // 
             // btnBook
@@ -186,7 +192,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(22, 89);
+            this.label3.Location = new System.Drawing.Point(22, 44);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(87, 21);
             this.label3.TabIndex = 14;
@@ -226,7 +232,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(397, 24);
+            this.label7.Location = new System.Drawing.Point(547, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(275, 30);
             this.label7.TabIndex = 18;
@@ -256,7 +262,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label9.Location = new System.Drawing.Point(21, 24);
+            this.label9.Location = new System.Drawing.Point(20, 0);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(306, 30);
             this.label9.TabIndex = 21;
@@ -284,6 +290,7 @@
             this.btnTopUp.TabIndex = 1;
             this.btnTopUp.Text = "Пополнить";
             this.btnTopUp.UseVisualStyleBackColor = false;
+            this.btnTopUp.Click += new System.EventHandler(this.btnTopUp_Click);
             // 
             // txtTopUpAmount
             // 
@@ -294,6 +301,9 @@
             // 
             // panelMain
             // 
+            this.panelMain.Controls.Add(this.label13);
+            this.panelMain.Controls.Add(this.dtpBookingTime);
+            this.panelMain.Controls.Add(this.dtpBookingDate);
             this.panelMain.Controls.Add(this.cmbHours);
             this.panelMain.Controls.Add(this.label10);
             this.panelMain.Controls.Add(this.label9);
@@ -315,13 +325,44 @@
             this.panelMain.Size = new System.Drawing.Size(1068, 411);
             this.panelMain.TabIndex = 23;
             // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(23, 92);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(105, 21);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "Дату и время";
+            // 
+            // dtpBookingTime
+            // 
+            this.dtpBookingTime.CustomFormat = "HH:mm";
+            this.dtpBookingTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpBookingTime.Location = new System.Drawing.Point(163, 92);
+            this.dtpBookingTime.Name = "dtpBookingTime";
+            this.dtpBookingTime.ShowUpDown = true;
+            this.dtpBookingTime.Size = new System.Drawing.Size(200, 20);
+            this.dtpBookingTime.TabIndex = 26;
+            this.dtpBookingTime.ValueChanged += new System.EventHandler(this.dtpBookingTime_ValueChanged);
+            // 
+            // dtpBookingDate
+            // 
+            this.dtpBookingDate.CustomFormat = "dd.MM.yyyy HH:mm";
+            this.dtpBookingDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBookingDate.Location = new System.Drawing.Point(163, 46);
+            this.dtpBookingDate.Name = "dtpBookingDate";
+            this.dtpBookingDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpBookingDate.TabIndex = 25;
+            this.dtpBookingDate.ValueChanged += new System.EventHandler(this.dtpBookingDate_ValueChanged);
+            // 
             // cmbHours
             // 
             this.cmbHours.Font = new System.Drawing.Font("Segoe UI", 8.25F);
             this.cmbHours.FormattingEnabled = true;
             this.cmbHours.Location = new System.Drawing.Point(164, 275);
             this.cmbHours.Name = "cmbHours";
-            this.cmbHours.Size = new System.Drawing.Size(121, 21);
+            this.cmbHours.Size = new System.Drawing.Size(199, 21);
             this.cmbHours.TabIndex = 24;
             this.cmbHours.SelectedIndexChanged += new System.EventHandler(this.cmbHours_SelectedIndexChanged);
             // 
@@ -335,12 +376,34 @@
             this.label10.TabIndex = 23;
             this.label10.Text = "Кол-во часов";
             // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(56, 86);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(78, 21);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Никнейм:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(56, 134);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(62, 21);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "Баланс:";
+            // 
             // CabinetForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.panelMain);
             this.Controls.Add(this.panelTopUp);
             this.Controls.Add(this.label2);
@@ -389,5 +452,10 @@
         private System.Windows.Forms.Button btnTopUp;
         private System.Windows.Forms.ComboBox cmbHours;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.DateTimePicker dtpBookingDate;
+        private System.Windows.Forms.DateTimePicker dtpBookingTime;
+        private System.Windows.Forms.Label label13;
     }
 }
